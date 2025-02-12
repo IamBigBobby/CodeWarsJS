0b8d245ac5b1e2679dbf6ada0324d813ce8bd6a0
Denis Svetleishii <denissvetleishii@outlook.com>
2025-02-12 18:13:14 +0400

    fix: fix balance

--- 6kyu/easyBalanceChecking.js
+++ 6kyu/easyBalanceChecking.js
@@ -30,52 +30,52 @@
 // R language: Don't use R's base function "mean()" that could give results slightly different from expected ones.
 
 function balance(book) {
-    const replacedBook = book.replaceAll(/[^a-zA-Z0-9\s.]/g, '');
-    const checkLine = replacedBook.split('\n').filter((line) => {
-        if(line !== ''){
-            return line;
-        }
-    });
+  const replacedBook = book.replaceAll(/[^a-zA-Z0-9\s.]/g, '');
+  const checkLine = replacedBook.split('\n').filter((line) => {
+      if(line !== ''){
+          return line;
+      }
+  });
 
-    if (!checkLine[0].startsWith('Original Balance: ')) {
-        const value = checkLine[0];
-        checkLine[0] = `Original Balance: ${value}`
-    }
-    
-    const numberOfDecimalPlaces = checkLine.map((line) => {
-        const arrLine = line.split(' ');
-        const value = Number(arrLine[arrLine.length - 1]);
-        arrLine[arrLine.length - 1] = value.toFixed(2).toString();
-        return arrLine.join(' ');
-    })
+  if (!checkLine[0].startsWith('Original Balance: ')) {
+      const value = checkLine[0];
+      checkLine[0] = `Original Balance: ${value}`
+  }
+  
+  const numberOfDecimalPlaces = checkLine.map((line) => {
+      const arrLine = line.split(' ');
+      const value = Number(arrLine[arrLine.length - 1]);
+      arrLine[arrLine.length - 1] = value.toFixed(2).toString();
+      return arrLine.join(' ');
+  })
 
-    const total = numberOfDecimalPlaces.reduce((acc, line, index) => {
-        if (index > 0) {
-            const splitLine = line.split(' ');
-            const value = Number(splitLine[splitLine.length - 1]);
-            return acc += value;
-        }
+  const total = numberOfDecimalPlaces.reduce((acc, line, index) => {
+      if (index > 0) {
+          const splitLine = line.split(' ');
+          const value = Number(splitLine[splitLine.length - 1]);
+          return acc += value;
+      }
 
-        return acc;
-    }, 0);
+      return acc;
+  }, 0);
 
-    const average = numberOfDecimalPlaces.length - 1;
+  const average = numberOfDecimalPlaces.length - 1;
 
-    let balance = 0;
-    const addingBalance = numberOfDecimalPlaces.map((line, index) => {
-        const splitLine = line.split(' ');
-        if (index === 0) {
-            balance = Number(splitLine[splitLine.length - 1]);
-            return splitLine.join(' ');
-        } else {
-            const value = splitLine[splitLine.length - 1];
-            balance -= value;
-            splitLine.push(`Balance ${(balance).toFixed(2)}`);
-            return splitLine.join(' ');
-        }
-    });
+  let balance = 0;
+  const addingBalance = numberOfDecimalPlaces.map((line, index) => {
+      const splitLine = line.split(' ');
+      if (index === 0) {
+          balance = Number(splitLine[splitLine.length - 1]);
+          return splitLine.join(' ');
+      } else {
+          const value = splitLine[splitLine.length - 1];
+          balance -= value;
+          splitLine.push(`Balance ${(balance).toFixed(2)}`);
+          return splitLine.join(' ');
+      }
+  });
 
-    return `${addingBalance.join('\r\n')}\r\nTotal expense  ${total.toFixed(2)}\r\nAverage expense  ${(total.toFixed(2) / average).toFixed(2)}`
+  return `${addingBalance.join('\r\n')}\r\nTotal expense  ${total.toFixed(2)}\r\nAverage expense  ${(total.toFixed(2) / average).toFixed(2)}`
 }
 
 console.log(balance(`1000.00!=
